75e1fbcef77b070cdc5dc53b8582b80e89bf201e
Vinicius Vieira <viniciusvieira@Viniciuss-MBP.lan>
2023-05-24 11:23:06 +0100

    feature: creation of modules, charts and github actions workflow

---
+++ demand-capacity-mgmt-backend/src/main/java/org/eclipse/tractusx/demandcapacitymgmt/demandcapacitymgmtbackend/exceptions/handler/RestExceptionHandler.java
@@ -0,0 +1,21 @@
+package org.eclipse.tractusx.demandcapacitymgmt.demandcapacitymgmtbackend.exceptions.handler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.eclipse.tractusx.demandcapacitymgmt.demandcapacitymgmtbackend.exceptions.ExceptionResponse;
+import org.eclipse.tractusx.demandcapacitymgmt.demandcapacitymgmtbackend.exceptions.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@Slf4j
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    public final ResponseEntity<Object> handleNotFoundException(NotFoundException ex) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exceptionResponse);
+    }
+}
---
+++ demand-capacity-mgmt-backend/src/main/java/org/eclipse/tractusx/demandcapacitymgmt/demandcapacitymgmtbackend/services/GreetingService.java
@@ -0,0 +1,10 @@
+package org.eclipse.tractusx.demandcapacitymgmt.demandcapacitymgmtbackend.services;
+
+import eclipse.tractusx.demand_capacity_mgmt_specification.model.Greeting;
+
+public interface GreetingService {
+
+    void saveGreeting(Greeting greeting);
+
+    void findById(String id);
+}
