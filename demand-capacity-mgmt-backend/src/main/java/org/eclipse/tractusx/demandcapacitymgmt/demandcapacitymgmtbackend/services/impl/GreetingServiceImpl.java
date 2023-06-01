a2edae1bb027c01cb0dbe4a5e1b69be075ed719f
Bailonis <p.bailao@campus.fct.unl.pt>
2023-06-01 15:15:53 +0100

    Update GreetingServiceImpl.java

--- demand-capacity-mgmt-backend/src/main/java/org/eclipse/tractusx/demandcapacitymgmt/demandcapacitymgmtbackend/services/impl/GreetingServiceImpl.java
+++ demand-capacity-mgmt-backend/src/main/java/org/eclipse/tractusx/demandcapacitymgmt/demandcapacitymgmtbackend/services/impl/GreetingServiceImpl.java
@@ -1,9 +1,17 @@
 package org.eclipse.tractusx.demandcapacitymgmt.demandcapacitymgmtbackend.services.impl;
 
 import eclipse.tractusx.demand_capacity_mgmt_specification.model.Greeting;
-import java.util.Optional;
+
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import org.eclipse.tractusx.demandcapacitymgmt.demandcapacitymgmtbackend.entities.DemandEntity;
+import org.eclipse.tractusx.demandcapacitymgmt.demandcapacitymgmtbackend.entities.ProjectEntity;
+import org.eclipse.tractusx.demandcapacitymgmt.demandcapacitymgmtbackend.entities.SupplierEntity;
+import org.eclipse.tractusx.demandcapacitymgmt.demandcapacitymgmtbackend.repositories.DemandRepository;
+import org.eclipse.tractusx.demandcapacitymgmt.demandcapacitymgmtbackend.repositories.ProjectRepository;
+import org.eclipse.tractusx.demandcapacitymgmt.demandcapacitymgmtbackend.repositories.SupplierRepository;
 import org.eclipse.tractusx.demandcapacitymgmt.demandcapacitymgmtbackend.services.GreetingService;
 import org.springframework.stereotype.Service;
 
@@ -12,12 +20,48 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class GreetingServiceImpl implements GreetingService {
 
-   
+    
+    private final ProjectRepository projectRepository;
+    private final SupplierRepository supplierRepository;
+    private final DemandRepository demandRepository;
+    
+
+    public ProjectRepository getProjectRepository() {
+        return projectRepository;
+    }
+
 
     @Override
     public void saveGreeting(Greeting greeting) {
-      
+
+        // Define the input for creating a project
+        ProjectEntity project = ProjectEntity.builder()
+                .id(1)
+                .name("Project 1")
+                .initial_date(LocalDateTime.now())
+                .build();
+        projectRepository.save(project);
+
+        // Define the input for creating a supplier
+        SupplierEntity supplier = SupplierEntity.builder()
+                .id(1)
+                .name("Supplier 1")
+                .build();
+        supplierRepository.save(supplier);
+
+        // Define the input for creating a demand
+        DemandEntity demand = DemandEntity.builder()
+                .id(1)
+                .project(project)
+                .supplier(supplier)
+                .requiredCapacity(2.0)
+                .actualDemand(4.0)
+                .description("description of demand")
+                .build();
+        demandRepository.save(demand);
+        
     }
+      
 
     @Override
     public void findById(String id) {}
