4fc46ea3cd03a52d3d8e18183af4d379b474a1a2
Vinicius Vieira <inicius_cbp@hotmail.com>
2023-06-05 14:34:59 +0100

    feature: created new endpoints

--- demand-capacity-mgmt-backend/src/main/java/org/eclipse/tractusx/demandcapacitymgmt/demandcapacitymgmtbackend/controllers/HelloController.java
+++ demand-capacity-mgmt-backend/src/main/java/org/eclipse/tractusx/demandcapacitymgmt/demandcapacitymgmtbackend/controllers/HelloController.java
@@ -1,17 +1,9 @@
 package org.eclipse.tractusx.demandcapacitymgmt.demandcapacitymgmtbackend.controllers;
 
-import eclipse.tractusx.demand_capacity_mgmt_specification.api.DefaultApi;
-import eclipse.tractusx.demand_capacity_mgmt_specification.model.Greeting;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
-public class HelloController implements DefaultApi {
-
-    @Override
-    public ResponseEntity<Greeting> getGreeting() {
-        return null;
-    }
-}
+public class HelloController {}
